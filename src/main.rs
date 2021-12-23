49671493d59dcd15a6d21a0b622946960129ba9d
Luke VanWingerden <lukeeverett16@gmail.com>
2021-12-23 16:32:17 -0500

    Added parser and  AST

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 mod scanner;
 mod token;
+mod ast;
+mod parser;
+
+use parser::Parser;
 
 fn main() {
     println!("Hello, Radish!");
+
+    let result = Parser::new("").parse();
+    println!("{:?}", result);
 }
