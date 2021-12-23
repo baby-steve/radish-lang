dde2c81b84496693d4f313a060b34eeb1cba20bd
Luke VanWingerden <lukeeverett16@gmail.com>
2021-12-23 16:39:46 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,6 @@ use parser::Parser;
 fn main() {
     println!("Hello, Radish!");
 
-    let result = Parser::new("").parse();
+    let result = Parser::new("12 + 3 - 45 * 678 / 9").parse();
     println!("{:?}", result);
 }
