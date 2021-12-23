63c756ff9a1ce23bcd1e93b4a8644fad059f8ee3
Luke VanWingerden <lukeeverett16@gmail.com>
2021-12-23 18:38:02 -0500

    Created opcode.rs

---
+++ src/opcode.rs
@@ -0,0 +1,20 @@
+#[repr(u8)]
+#[derive(Debug, PartialEq, Eq)]
+pub enum Opcode {
+    Constant,
+
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+
+    Return,
+}
+
+impl From<u8> for Opcode {
+    /// convert a raw byte into an opcode.
+    /// Note that non-opcode bytes should never be interpreted as opcodes (will break).
+    fn from(op: u8) -> Opcode {
+        unsafe { std::mem::transmute(op) }
+    }
+}
