a68005b5e562f93f8a9058d146679301e2bb3d90
Luke VanWingerden <lukeeverett16@gmail.com>
2021-12-23 21:15:17 -0500

    Added Value type

---
+++ src/value.rs
@@ -0,0 +1,43 @@
+use std::ops::{Add, Sub, Mul, Div};
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Value {
+    Number(f64),
+}
+
+impl Add for Value {
+    type Output = Self;
+    fn add(self, other: Value) -> <Self as std::ops::Add<Value>>::Output {
+        match (self, other) {
+            (Value::Number(a), Value::Number(b)) => return Value::Number(a + b),
+        }
+    }
+}
+
+impl Sub for Value {
+    type Output = Self;
+    fn sub(self, other: Value) -> <Self as std::ops::Sub<Value>>::Output {
+        match (self, other) {
+            (Value::Number(a), Value::Number(b)) => Value::Number(a - b),
+        }
+    }
+}
+
+impl Mul for Value {
+    type Output = Self;
+    fn mul(self, other: Value) -> <Self as std::ops::Mul<Value>>::Output {
+        match (self, other) {
+            (Value::Number(a), Value::Number(b)) => Value::Number(a * b), 
+        }
+    }
+}
+
+
+impl Div for Value {
+    type Output = Self;
+    fn div(self, other: Value) -> <Self as std::ops::Div<Value>>::Output {
+        match (self, other) {
+            (Value::Number(a), Value::Number(b)) => Value::Number(a / b), 
+        }
+    }
+}
