42d8e1747338c689c98c84ce0f9eeeecb037b267
Luke VanWingerden <lukeeverett16@gmail.com>
2021-12-26 13:12:40 -0500

    Rudimentarily piped compiler to vm

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,18 @@ mod vm;
 
 use parser::Parser;
 use compiler::Compiler;
+use vm::VM;
 
 fn main() {
     println!("Hello, Radish!");
 
-    let result = Parser::new("1 + 23 - 4 * 56 / 7").parse().unwrap();
+    let result = Parser::new("12 + 7 - 8 / 4 * 5").parse().unwrap();
     println!("{:#?}", result);
 
     let mut compiler = Compiler::new();
     compiler.run(&result);
     println!("{:?}", compiler.chunk);
+
+    let mut vm = VM::new(compiler.chunk);
+    vm.interpret();
 }
